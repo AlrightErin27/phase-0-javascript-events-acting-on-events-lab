397c94e7521cb40e113653da5734b7f175c96b3a
Erin Van Brunt <erinmontybruce@gmail.com>
2022-02-06 17:34:31 -0800

    fin

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Your code here
+const dodger = document.getElementById(`dodger`);
+
+function moveDodgerLeft() {
+  const leftNum = dodger.style.left.replace(`px`, ``);
+  const left = parseInt(leftNum, 10);
+  if (left > 0) dodger.style.left = `${left - 1}px`;
+}
+
+function moveDodgerRight() {
+  const leftNum = dodger.style.left.replace(`px`, ``);
+  const left = parseInt(leftNum, 10);
+  if (left < 360) dodger.style.left = `${left + 1}px`;
+}
+
+document.addEventListener(`keydown`, function (event) {
+  if (event.key === `ArrowLeft`) moveDodgerLeft() && console.log(`Moved Left`);
+  if (event.key === `ArrowRight`)
+    moveDodgerRight() && console.log(`Moved Right`);
+});
