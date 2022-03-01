026ef3900edac4353a88edab334cbdb8a28d2eee
Erin Van Brunt <erinmontybruce@gmail.com>
2022-03-01 11:01:11 -0800

    half through form lab

--- index.js
+++ index.js
@@ -1,44 +1,56 @@
-const dodger = document.getElementById("dodger");
-dodger.style.backgroundColor = "#FF5733";
-// dodger.style.bottom = "0px";
-// dodger.style.left = "0px";
+document.addEventListener("DOMContentLoaded", () => {
+  const dodger = document.getElementById("dodger");
+  dodger.style.backgroundColor = "#FF5733";
+  // dodger.style.bottom = "0px";
+  // dodger.style.left = "0px";
 
-// document.addEventListener("keydown", function (e) {
-//   console.log(e);
-// });
+  // document.addEventListener("keydown", function (e) {
+  //   console.log(e);
+  // });
 
-// document.addEventListener("keydown", function (e) {
-//   if (e.key === "ArrowLeft") {
-//     const leftNum = dodger.style.left.replace("px", "");
-//     const left = parseInt(leftNum, 10);
+  // document.addEventListener("keydown", function (e) {
+  //   if (e.key === "ArrowLeft") {
 
-//     dodger.style.left = `${left - 10}px`;
-//     console.log(dodger.style.left);
-//   }
-// });
+  //     const leftNum = dodger.style.left.replace("px", "");
+  //     const left = parseInt(leftNum, 10);
 
-function moveDodgerLeft() {
-  const leftNum = dodger.style.left.replace("px", "");
-  const left = parseInt(leftNum, 10);
+  //     dodger.style.left = `${left - 10}px`;
+  //     console.log(dodger.style.left);
+  //   }
+  // });
 
-  if (left > 0) {
-    dodger.style.left = `${left - 10}px`;
+  function moveDodgerLeft() {
+    const leftNum = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNum, 10);
+
+    if (left > 0) {
+      dodger.style.left = `${left - 10}px`;
+    }
   }
-}
-function moveDodgerRight() {
-  const rightNum = dodger.style.left.replace("px", "");
-  const left = parseInt(rightNum, 10);
+  function moveDodgerRight() {
+    const rightNum = dodger.style.left.replace("px", "");
+    const left = parseInt(rightNum, 10);
 
-  if (left < 360) {
-    dodger.style.left = `${left + 10}px`;
+    if (left < 360) {
+      dodger.style.left = `${left + 10}px`;
+    }
   }
-}
 
-document.addEventListener("keydown", function (e) {
-  if (e.key === "ArrowLeft") {
-    moveDodgerLeft();
-  }
-  if (e.key === "ArrowRight") {
-    moveDodgerRight();
-  }
+  document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowLeft") {
+      moveDodgerLeft();
+    }
+    if (e.key === "ArrowRight") {
+      moveDodgerRight();
+    }
+  });
+
+  // FORM LAB STARTING HERE---->
+  document.querySelector("form").addEventListener("submit", (e) => {
+    //b/c of how forms were originally built- they automatically refresh the page when
+    //the event happens(submit button is pressed). Below is code that prevents this:
+    e.preventDefault();
+    console.log(e.target.value);
+    // handleToDO(e.target.new_todo.value);
+  });
 });
